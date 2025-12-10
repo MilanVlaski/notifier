bb2c321337709029934aa702266b915187ab8397
Milan Vlaški <milan.vlaski321@gmail.com>
2025-12-10 13:48:22 +0100

    refactor: remove duplicate build stuff in web/build.gradle.kts

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -8,24 +8,9 @@ group = "com.akimi"
 version = "0.0.1-SNAPSHOT"
 description = "Message "
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
