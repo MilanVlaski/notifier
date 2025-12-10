7ff9d98ae0547c3723cf306f81bc4af0b36a4071
Milan Vlaški <milan.vlaski321@gmail.com>
2025-12-10 15:15:15 +0100

    refactor: move spring boot version to toml

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
     id("java-common-conventions")
-    // TODO hardcoded version
-    id("org.springframework.boot") version "4.0.0"
+    alias(libs.plugins.spring.boot)
 }
 
 description = "Message "
 
 dependencies {
-    // TODO hardcoded version
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:4.0.0"));
+    // Recommended over Spring Dependency management by the creator, see: https://www.linen.dev/s/gradle-community/t/2579116/what-is-the-proper-way-to-apply-a-bom-in-a-library-project-i#a7d4a60a-ab60-48dc-8477-f3f4462d1e6a
+    implementation(platform(libs.spring.boot.deps));
 
     implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
