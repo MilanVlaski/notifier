89de3e1cbbd0803cd1ad5c4dc44bb114d05732ad
Milan Vlaški <milan.vlaski321@gmail.com>
2025-12-14 12:59:18 +0100

    refactor: rename

--- web/src/test/java/com/akimi/notifier/web/SpringBootSanityTest.java
+++ web/src/test/java/com/akimi/notifier/web/SpringBootSanityTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotifierApplicationTests {
+class SpringBootSanityTest {
 
     @Test
     void contextLoads() {
